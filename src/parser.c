b0508a10718b4348ddc6811cec117746ac4d8e65
rzfann <fajrulsyaban47@gmail.com>
2026-04-13 20:25:18 +0700

    Update : Update Src Folder

--- src/parser.c
+++ src/parser.c
@@ -7797,7 +7797,7 @@ extern "C" {
 #define TS_PUBLIC __attribute__((visibility("default")))
 #endif
 
-TS_PUBLIC const TSLanguage *tree_sitter_samalang(void) {
+TS_PUBLIC const TSLanguage *tree_sitter_samawa_language(void) {
   static const TSLanguage language = {
     .abi_version = LANGUAGE_VERSION,
     .symbol_count = SYMBOL_COUNT,
@@ -7822,7 +7822,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_samalang(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
-    .name = "samalang",
+    .name = "samawa_language",
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
